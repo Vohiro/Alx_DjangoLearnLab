244d59202bb53182eaaeb55407e3b2915e45b97e
Vohiro <dvohiro@gmail.com>
2025-07-15 00:24:09 +0100

    implementing both function-based and class-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from relationship_app import views
+from .views import list_books
 #from django.views.generic import DetailView
 
 urlpatterns=[
-    path('display-book/', views.display_book_list, name='Display Book List'),
+    path('display-book/', list_books, name='Display Book List'),
     path('library-detail/<int:pk>/', views.LibraryDetailView.as_view(), name='library detail'),
          ]
