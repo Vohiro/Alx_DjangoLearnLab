4520e56a272554d79b28316a19174ba1ad1a3ca1
Vohiro <dvohiro@gmail.com>
2025-07-15 10:38:08 +0100

    demonstrating how django manages user session and permissions serving userlogin, logout and registration views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from .views import list_books
-#from django.views.generic import DetailView
+from django.urls import path, include
+from .views import list_books, LibraryDetailView, LibraryListView, RegisterView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns=[
     path('display-book/', list_books, name='Display Book List'),
-    path('library-detail/<int:pk>/', views.LibraryDetailView.as_view(), name='library detail'),
+    path('library-detail/', LibraryListView.as_view(), name='library list detail'),
+    path('library-detail/<int:pk>/', LibraryDetailView.as_view(), name='library detail'),
+    path('register/', RegisterView.as_view(), name='register' ),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationshipp_app/logout.html'), name='logout'), 
          ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render
 from .models import Library, Book, Author, Librarian
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-def display_book_list(request):
+def list_books(request):
     """ Retrieves all books and display all book titles and their authors"""
     books = Book.objects.all()
     context = { 'book_list': books }
     return render(request, 'relationship_app/list_books.html', context)
 
 
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_list.html'
+
+
 class LibraryDetailView(DetailView):
     """ A class based view for displaying list details of a specific library """
     model = Library
     template_name = "relationship_app/library_detail.html"
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+    def form_valid(self, form):
+        # CreateView saves the new user and returns it
+        response = super().form_valid(form)
+        user = self.object
+
+        # Login the created/new user automatically
+        login(self.request, user)
+
+        # retun the redirect response
+        return response
+
+
