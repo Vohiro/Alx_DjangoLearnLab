90cdd1295c5ebf170ae4d739ca1a8f17b7f88e96
Vohiro <dvohiro@gmail.com>
2025-07-15 10:50:05 +0100

    demonstrating how django manages user session and permissions serving userlogin, logout and registration views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import list_books, LibraryDetailView, LibraryListView, RegisterView
+from .views import list_books, LibraryDetailView, LibraryListView
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns=[
     path('display-book/', list_books, name='Display Book List'),
     path('library-detail/', LibraryListView.as_view(), name='library list detail'),
     path('library-detail/<int:pk>/', LibraryDetailView.as_view(), name='library detail'),
-    path('register/', RegisterView.as_view(), name='register' ),
+    path('register/', views.RegisterView.as_view(), name='register' ),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationshipp_app/logout.html'), name='logout'), 
          ]
