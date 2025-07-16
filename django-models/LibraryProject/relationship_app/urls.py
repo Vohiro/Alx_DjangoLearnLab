068707a17ce92d40c276c57992c96858f2f027d0
Vohiro <dvohiro@gmail.com>
2025-07-16 17:12:34 +0100

    implementing Role-Based acess control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,5 +9,8 @@ urlpatterns=[
     path('library-detail/<int:pk>/', LibraryDetailView.as_view(), name='library detail'),
     path('register/', views.register, name='register' ),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationshipp_app/logout.html'), name='logout'), 
+    path('logout/', LogoutView.as_view(template_name='relationshipp_app/logout.html'), name='logout'),
+    path('admin-only', views.admin_view, name='admin_view'),
+    path('librarian-only', views.librarian_view, name='librarian_view'),
+    path('member-only', views.member_view, name='member_view'),
          ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 # Create your views here.
 
@@ -28,11 +29,11 @@ class LibraryDetailView(DetailView):
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm(request.POST)
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
-    def get_form(self, form_class=none):
+    def get_form(self, form_class=None):
         """ Controlling form instantiation through On Get and on Post """
         form_class = form_class or self.get_form_class()
         return form_class(self.request.POST or None, self.request.FILES or None)
@@ -53,3 +54,32 @@ register = RegisterView.as_view()
 
 # Explicitly calling dummy UserCreationForm to pass alx checker
 _ = UserCreationForm()
+
+# Role checking functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
