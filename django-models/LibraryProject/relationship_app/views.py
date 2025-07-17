b51cd135f5456cf9b1a91d7b2d1161d54e93e3d6
Vohiro <dvohiro@gmail.com>
2025-07-17 16:54:16 +0100

    implementing custom permission

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import user_passes_test, login_required, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test, login_required
 
 # Create your views here.
 
