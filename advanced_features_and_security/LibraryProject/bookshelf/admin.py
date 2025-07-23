38f2451f4be2002752327f6f5347ed7617e181a2
Vohiro <dvohiro@gmail.com>
2025-07-23 16:42:38 +0100

    Implementing a custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy
 
 # Register your models here.
 
@@ -9,4 +12,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ['publication_year']
     search_fields = ['title', 'author']
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['Username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (gettext_lazy('Additional info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (gettext_lazy('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
