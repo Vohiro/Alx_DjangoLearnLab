8113b13c16534e2a927bb9e18fb101f2de31f067
Vohiro <dvohiro@gmail.com>
2025-08-08 18:29:09 +0100

    setting up the models and serializer with custom validation

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party libraries
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
