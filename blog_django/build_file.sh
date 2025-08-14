e9469517ed7dd7c44ebe44d51b72ff38e63600bc
Vohiro <dvohiro@gmail.com>
2025-08-14 09:37:49 +0100

    vercel production settings

--- blog_django/build_file.sh
+++ blog_django/build_file.sh
@@ -1,3 +1,3 @@
-pip install -r requirements.txt
-python manage.py collectstatic
-python manage.py migrate
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic
+python3 manage.py migrate
