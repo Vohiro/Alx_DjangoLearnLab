f2575dc68aae9b7de8bc0ca47a87528203a23c3a
Vohiro <dvohiro@gmail.com>
2025-08-14 08:45:10 +0100

    vercel production settings

--- blog_django/django_blog/settings.py
+++ blog_django/django_blog/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'staticfiles_build')
 STORAGES = {
     "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
 }
