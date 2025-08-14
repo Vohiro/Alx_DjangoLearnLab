04990164c2153897f57fd3c19adcaf59b9f98cfa
Vohiro <dvohiro@gmail.com>
2025-08-14 11:15:14 +0100

    vercel production settings

--- blog_django/django_blog/settings.py
+++ blog_django/django_blog/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*.onrender.com", 'localhost', "127.0.0.1", "*.vercel.app"]
-CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com", "https://*.vercel.app"]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 
 # Application definition
