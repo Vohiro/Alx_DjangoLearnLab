edac241f79d474894b8443ce11235a126a01c201
Vohiro <dvohiro@gmail.com>
2025-08-21 13:13:31 +0100

    Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(read_only=True, source='author')
+    comment_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "author", "author_id", "title", "content",
+                  "created_at", "updated_at", "comments_count"]
+        read_only_fields = ["id", "author", "author_id", "created_at", "updated_at", "comments_count"]
+
+        def to_representation(self, instance):
+            # Efficiently annotate count if not provided by queryset
+            rep = super().to_representation(instance)
+            if "comments_count" not in rep or rep["comments_count"] is None:
+                rep["comments_count"] = instance.comments.count()
+            return rep
+        
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(
+        read_only=True, source="author"
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["id", "post", "author", "author_id", "content", "created_at", "updated_at"]
+        read_only_fields = ["id", "author", "author_id", "created_at", "updated_at"]
---
+++ social_media_api/posts/views.py
@@ -0,0 +1,59 @@
+from rest_framework import viewsets
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+from django.db.models import Count
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filterset_fields = ["author"]            # ?author=<user_id>
+    search_fields = ["title", "content"]     # ?search=keyword
+    ordering_fields = ["created_at", "updated_at", "title"]  # ?ordering=-created_at
+
+    def get_queryset(self):
+        return Post.objects.select_related("author").annotate(
+            comments_count=Count("comments")
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        # ensure author can't be changed
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=["get"])
+    def comments(self, request, pk=None):
+        """
+        Optional convenience sub-route: /posts/<id>/comments/
+        """
+        post = self.get_object()
+        qs = post.comments.select_related("author").all()
+        page = self.paginate_queryset(qs)
+        ser = CommentSerializer(page or qs, many=True)
+        if page is not None:
+            return self.get_paginated_response(ser.data)
+        return Response(ser.data)
+    
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filterset_fields = ["post", "author"]    # ?post=<post_id>
+    search_fields = ["content"]              # ?search=keyword
+    ordering_fields = ["created_at", "updated_at"]
+
+    def get_queryset(self):
+        qs = Comment.objects.select_related("post", "author")
+        post_id = self.request.query_params.get("post")
+        if post_id:
+            qs = qs.filter(post_id=post_id)
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
